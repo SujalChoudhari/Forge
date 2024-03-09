b93f75197103e5e1ac0590653a925a8fc36e7128
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-03-09 22:34:57 +0530

    Adds nameless, keywords, flags to arg parser

--- src/argparser.rs
+++ src/argparser.rs
@@ -1,7 +1,37 @@
-pub fn load_args() -> Vec<String> {
-    let mut args: Vec<String> = Vec::new();
+use std::collections::HashMap;
+#[derive(Debug)]
+pub struct Arguments {
+    nameless: Vec<String>,
+    flags: Vec<String>,
+    keword_arguments: HashMap<String, String>,
+}
+
+pub fn load_command_line_arguents() -> Arguments {
+    let mut args = Arguments {
+        nameless: Vec::new(),
+        flags: Vec::new(),
+        keword_arguments: HashMap::new(),
+    };
+
     for individual_args in std::env::args() {
-        args.push(individual_args);
+        if individual_args.starts_with("--") {
+            // keyword
+            if let Some(split_args) = individual_args.split_once("=") {
+                args.keword_arguments.insert(
+                    split_args.0.replace("--", "").to_owned(),
+                    split_args.1.to_owned(),
+                );
+            }
+        } else if individual_args.starts_with("-") {
+            // flags
+            args.flags.push(individual_args.replace("-", ""));
+        } else {
+            // nameless arg
+            args.nameless.push(individual_args);
+        }
     }
+
+    println!("{:?}", args);
+
     args
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use argparser::load_args;
+use argparser::load_command_line_arguents;
 
 use crate::parser::load_forge;
 
@@ -7,15 +7,15 @@ pub mod logging;
 pub mod parser;
 pub mod argparser;
 fn main() {
-    // let data = load_forge("./examples/forge");
-    // let job = interpreter::get_job(data, "build".to_string());
-    // let variables: std::collections::HashMap<String, Vec<String>> = interpreter::get_variables(&job);
-    // let os = interpreter::get_operating_systems(&job);
-    // let deps = interpreter::get_dependencies(&job);
-    // let com = interpreter::get_commands(&job);
-    // println!("{:?}",variables);
-    // println!("{:?}",os);
-    // println!("{:?}",deps);
-    // println!("{:?}",com);
-    load_args();
+    let data = load_forge("./examples/forge");
+    let job = interpreter::get_job(data, "build".to_string());
+    let variables: std::collections::HashMap<String, Vec<String>> = interpreter::get_variables(&job);
+    let os = interpreter::get_operating_systems(&job);
+    let deps = interpreter::get_dependencies(&job);
+    let com = interpreter::get_commands(&job);
+    println!("{:?}",variables);
+    println!("{:?}",os);
+    println!("{:?}",deps);
+    println!("{:?}",com);
+    load_command_line_arguents();
 }
