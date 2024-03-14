2cc2595225fdb1ae4af3bcf335418e714279d394
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-03-14 16:37:10 +0530

    Logger updated to get input itself.

--- crates/logger/src/lib.rs
+++ crates/logger/src/lib.rs
@@ -1,9 +1,8 @@
 use colored::{ColoredString, Colorize};
-use std::process::exit;
-
 use constants::{
     APP_NAME, APP_SUBTITLE, APP_VERSION, ERROR_TAG, INFORMATION_TAG, INPUT_TAG, WARNING_TAG,
 };
+use std::io::{self, Write};
 
 #[derive(Debug)]
 enum LogType {
@@ -20,14 +19,17 @@ pub fn error(message: &str) {
 }
 
 pub fn start() {
-    let title = APP_NAME.truecolor(0, 0, 0).bold().on_bright_cyan();
-    let version = if unsafe { IS_VERBOSE } {
-        APP_VERSION.truecolor(100, 100, 100).italic()
-    } else {
-        "".bold()
-    };
-    let message = APP_SUBTITLE.truecolor(90, 90, 90).italic();
-    println!(" {} {} {}\n", title, version, message);
+    let title = format!(
+        " {} {} {}",
+        APP_NAME.truecolor(0, 0, 0).bold().on_bright_cyan(),
+        if unsafe { IS_VERBOSE } {
+            APP_VERSION.truecolor(100, 100, 100).italic()
+        } else {
+            "".bold()
+        },
+        APP_SUBTITLE.truecolor(90, 90, 90).italic()
+    );
+    println!("{}\n", title);
 }
 
 pub fn warn(message: &str) {
@@ -38,76 +40,102 @@ pub fn info(message: &str) {
     log(LogType::INFO, message);
 }
 
-pub fn input(message: &str) {
-    log(LogType::INPUT, message);
+pub fn input(prompt: &str, default: &str) -> String {
+    let info_tag = get_log_tag(&LogType::INPUT);
+    let time_string = get_time_colored();
+    print!(
+        "{info_tag} {time_string}: {} ({}) >> ",
+        prompt,
+        default.truecolor(100, 100, 100)
+    );
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let input = input.trim();
+    if input.is_empty() {
+        default.to_string()
+    } else {
+        input.to_string()
+    }
 }
 
 pub fn intermidiate_info(title: &str, message: &str) {
-    let content = message.truecolor(40, 40, 40);
-    intermidiate_log(LogType::INFO, title.white().bold(), content);
+    intermidiate_log(
+        LogType::INFO,
+        title.white().bold(),
+        message.truecolor(40, 40, 40),
+    );
 }
 
 pub fn intermidiate_error(title: &str, message: &str) {
-    let content = message.truecolor(150, 0, 0);
-    intermidiate_log(LogType::ERROR, title.white().bold(), content);
+    intermidiate_log(
+        LogType::ERROR,
+        title.white().bold(),
+        message.truecolor(150, 0, 0),
+    );
+}
+
+fn get_time_colored() -> ColoredString {
+    if unsafe { IS_VERBOSE } {
+        get_time().italic().magenta()
+    } else {
+        "".bold()
+    }
 }
 
 fn get_time() -> String {
     let current_datetime = chrono::Local::now();
-    let formatted_time = current_datetime.format("%H:%M:%S%.3f").to_string();
-    formatted_time
+    current_datetime.format("%H:%M:%S%.3f").to_string()
 }
 
 fn get_log_tag(log_type: &LogType) -> ColoredString {
-    let info_tag = match log_type {
-        LogType::INFO => INFORMATION_TAG
-            .bold()
-            .on_truecolor(0, 255, 0)
-            .truecolor(0, 0, 0),
-        LogType::INPUT => INPUT_TAG
-            .bold()
-            .on_truecolor(0, 150, 255)
-            .truecolor(0, 0, 0),
-        LogType::WARN => WARNING_TAG
-            .bold()
-            .on_truecolor(0, 255, 255)
-            .truecolor(0, 0, 0),
-        LogType::ERROR => ERROR_TAG.bold().on_truecolor(255, 0, 0).truecolor(0, 0, 0),
+    let (tag, color) = match log_type {
+        LogType::INFO => (INFORMATION_TAG, (0, 255, 0)),
+        LogType::INPUT => (INPUT_TAG, (0, 150, 255)),
+        LogType::WARN => (WARNING_TAG, (0, 255, 255)),
+        LogType::ERROR => (ERROR_TAG, (255, 0, 0)),
     };
-    info_tag
+    tag.bold()
+        .on_truecolor(color.0, color.1, color.2)
+        .truecolor(0, 0, 0)
 }
 
 fn log(log_type: LogType, message: &str) {
     let info_tag = get_log_tag(&log_type);
-    let time_string: colored::ColoredString = if unsafe { IS_VERBOSE } {
-        get_time().italic().magenta()
-    } else {
-        "".bold()
-    };
-
+    let time_string = get_time_colored();
     match log_type {
-        LogType::INFO => {
-            println!("{info_tag} {time_string}: {message}");
-        }
-        LogType::INPUT => {
-            println!("{info_tag} {time_string}: {message}");
-        }
-        LogType::WARN => {
-            eprintln!("{info_tag} {time_string}: {message}");
-        }
-        LogType::ERROR => {
-            eprintln!("{info_tag} {time_string}: {message}");
-            exit(0);
-        }
+        LogType::INFO => println!(
+            "{info_tag} {time_string}: {message}",
+            info_tag = info_tag,
+            time_string = time_string,
+            message = message
+        ),
+        LogType::INPUT => (), // nothing to log for input
+        LogType::WARN | LogType::ERROR => eprintln!(
+            "{info_tag} {time_string}: {message}",
+            info_tag = info_tag,
+            time_string = time_string,
+            message = message
+        ),
     }
 }
 
 fn intermidiate_log(log_type: LogType, title: ColoredString, message: ColoredString) {
     let info_tag = get_log_tag(&log_type);
-    let time_string: colored::ColoredString = get_time().italic().magenta();
+    let time_string = get_time_colored();
     if unsafe { IS_VERBOSE } {
-        println!("{info_tag} {time_string}:\n{title}\n{message}");
+        println!(
+            "{info_tag} {time_string}:\n{title}\n{message}",
+            info_tag = info_tag,
+            time_string = time_string,
+            title = title,
+            message = message
+        );
     } else {
-        println!("{info_tag}: {title}");
+        println!("{info_tag}: {title}", info_tag = info_tag, title = title);
     }
 }
